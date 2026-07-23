cdd557b33455fce0d5f68cedd99cdd3575c2ec17
Saurabh Negi <itssaurabh.negi@gmail.com>
2026-07-23 11:19:00 +0000

    new

--- ansible/files/install-docker.sh
+++ ansible/files/install-docker.sh
@@ -9,7 +9,7 @@ apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-  | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 
 chmod a+r /etc/apt/keyrings/docker.gpg
 
